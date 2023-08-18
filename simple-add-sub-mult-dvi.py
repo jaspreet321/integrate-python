cbb1f7c5be6c0bb1d54488118d5f1f58a73f4d89
Jaspreet Matharu Singh <57534913+jaspreet321@users.noreply.github.com>
2023-08-18 07:44:44 +0000

    adding new file

---
+++ simple-add-sub-mult-dvi.py
@@ -0,0 +1,10 @@
+sum1 = 3
+
+sum2 = 5
+
+addition = int(sum1) + int(sum2)
+substract = int(sum2) - int(sum1)
+multiply = int(sum1) * int(sum2)
+divide = int(sum2) / int(sum1)
+
+print (addition, substract, multiply, divide)
