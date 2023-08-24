6bb26b68d60fcc1b23df6106d2f9c584e6cb0400
Jaspreet Matharu Singh <57534913+jaspreet321@users.noreply.github.com>
2023-08-24 16:35:12 +0000

    saving files

---
+++ input_manual_function.py
@@ -0,0 +1,13 @@
+#input function will allow the user to manually enter the value while execution
+xyz = input("Enter your name here: ")
+rfg = input("Enter your dob: ")
+yhg = input("Enter your phone number: ")
+
+print(xyz)
+print(type(xyz))
+
+print(rfg)
+print(type(rfg))
+
+print(yhg)
+print(type(yhg))
