eb9b73418555f3af617451d46c297e02930642bd
Jaspreet Matharu Singh <57534913+jaspreet321@users.noreply.github.com>
2023-09-12 10:16:38 +0000

    saving changes

---
+++ string-slicing.py
@@ -0,0 +1,18 @@
+name = "validation"
+
+#print specific letter from the word, by calling specific number/location of the alphabate in the line/word
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+print(name[8])
+print(name[9])
+
+# you can also call a series of letters from the line/word'
+print(name[0:2])
+print(name[1:5])
+print(name[2:8])
