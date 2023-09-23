8ea825f5e6a09dcf7391f055c4085bc23d96526e
Jaspreet Matharu Singh <57534913+jaspreet321@users.noreply.github.com>
2023-09-23 14:00:22 +0000

    new codes

---
+++ set_logic.py
@@ -0,0 +1,7 @@
+b = {1} #initialize value 2 in variable 'b' - remember this is a 'SET' because value is in {}
+
+print(b) #print the value of b
+
+b.add(5) #this will add number 4 in the sequence, output will be {1,5}
+
+print(b)
