575e4f8f1f03417cbe1d69a4fe2929d69724e635
Jaspreet Matharu Singh <57534913+jaspreet321@users.noreply.github.com>
2023-10-02 05:28:46 +0000

    files

---
+++ py_if_else.py
@@ -0,0 +1,8 @@
+xyz = 99
+
+if(xyz == 00):
+    print("Value is not satisfied")
+elif(xyz == 23):
+    print("Value is again not satisfied")
+else:
+    print("Actual value is 9")
---
+++ py_multiple_if.py
@@ -0,0 +1,10 @@
+first = 40
+second = 50
+third = 70
+
+if(first == 40):
+    print("Value of first is 40")
+if(second == 50):
+    print("value of second is 50")
+if(third == 70):
+    print("value of third is 70")
