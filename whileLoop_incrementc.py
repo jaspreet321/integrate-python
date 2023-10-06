45b8164c04650a2e1ba5b37430a44dc01579ac6d
Jaspreet Matharu Singh <57534913+jaspreet321@users.noreply.github.com>
2023-10-06 08:27:29 +0000

    file add

---
+++ whileLoop_incrementc.py
@@ -0,0 +1,10 @@
+#read whole script thoroughly
+#Intentations/spacing is very imp in WHILE command, see below where the PRINT command & 'i' is mentioned.
+
+
+
+i = 1                        #initializing value of "i" as 1
+
+while i<=50:                 #this line will check if "i" is less than or equal to 50
+    print("testing")         #if above line is turns out to be true, then this print line keep on printing testing without stopping 
+    i = i + 1                #this line will start counting i from number 1 & goes till 50, once i becomes 51 then 'testing' will stop pri                             nting, because as stated above while loop will only run until i is less than or eaquals to 50.       
