7e88972af4fcd68cb24fc1bd2dedb3ab1482458d
Jaspreet Matharu Singh <57534913+jaspreet321@users.noreply.github.com>
2023-10-09 13:28:13 +0000

    files

---
+++ forLoop_ex.py
@@ -0,0 +1,8 @@
+#IMP - FOR loop does the same work as WHILE loop does, it's just that FOR loop is bit less complicated than WHILE
+
+#Let's draft the list of fruits under variable 'fruits' & then print that list using FOR loop
+
+fruits = ['banana', 'apple', 'pineapple']
+
+for item in fruits:          #'item' is just the way in FOR loop to call the entire list                    
+    print(item)              
